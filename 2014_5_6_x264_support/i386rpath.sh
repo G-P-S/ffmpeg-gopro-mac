f9bb5fdf8bae109dfd075832961df40d3f7efd79
Josh Bodinet <jbodinet@gopro.com>
2014-06-04 19:17:03 -0700

    fixed ffmpeg i386 x264 rpathed binaries so as to be able to load properly in TestApp
    fixed i386rpath.sh so as to properly set up binaries

--- 2014_5_6_x264_support/i386rpath.sh
+++ 2014_5_6_x264_support/i386rpath.sh
@@ -1,18 +1,37 @@
-# cd to the proper directory
-cd i386/lib/rpathed
+# cd to the i386/lib directory
+cd i386/lib
+
+# recursively delete any existing rpathed directory
+if [ -d rpathed ]
+then
+echo removing existing rpathed directory
+rm -r rpathed
+fi
+
+# create a new rpathed directory
+mkdir rpathed
+
+# copy (and rename) necessary dylibs
+cp libavcodec.55.60.103.dylib rpathed/libavcodec.55.dylib
+cp libavformat.55.37.102.dylib rpathed/libavformat.55.dylib
+cp libavutil.52.81.100.dylib rpathed/libavutil.52.dylib
+cp libswscale.2.6.100.dylib rpathed/libswscale.2.dylib
+
+# cd to the new rpathed directory
+cd rpathed
 
 # libavcodec
-install_name_tool -id @rpath/../Frameworks/libavcodec.55.60.103.dylib libavcodec.55.60.103.dylib
-install_name_tool -change i386/lib/libavutil.52.dylib @rpath/../Frameworks/libavutil.52.dylib libavcodec.55.60.103.dylib
+install_name_tool -id @rpath/../Frameworks/libavcodec.55.dylib libavcodec.55.dylib
+install_name_tool -change i386/lib/libavutil.52.dylib @rpath/../Frameworks/libavutil.52.dylib libavcodec.55.dylib
 
 # libavformat
-install_name_tool -id @rpath/../Frameworks/libavformat.55.37.102.dylib libavformat.55.37.102.dylib
-install_name_tool -change i386/lib/libavcodec.55.dylib @rpath/../Frameworks/libavcodec.55.dylib libavformat.55.37.102.dylib
-install_name_tool -change i386/lib/libavutil.52.dylib @rpath/../Frameworks/libavutil.52.dylib libavformat.55.37.102.dylib
+install_name_tool -id @rpath/../Frameworks/libavformat.55.dylib libavformat.55.dylib
+install_name_tool -change i386/lib/libavcodec.55.dylib @rpath/../Frameworks/libavcodec.55.dylib libavformat.55.dylib
+install_name_tool -change i386/lib/libavutil.52.dylib @rpath/../Frameworks/libavutil.52.dylib libavformat.55.dylib
 
 # libavutil
-install_name_tool -id @rpath/../Frameworks/libavutil.52.81.100.dylib libavutil.52.81.100.dylib
+install_name_tool -id @rpath/../Frameworks/libavutil.52.dylib libavutil.52.dylib
 
 # libsyscale
-install_name_tool -id @rpath/../Frameworks/libswscale.2.6.100.dylib libswscale.2.6.100.dylib
-install_name_tool -change i386/lib/libavutil.52.dylib @rpath/../Frameworks/libavutil.52.dylib libswscale.2.6.100.dylib
+install_name_tool -id @rpath/../Frameworks/libswscale.2.dylib libswscale.2.dylib
+install_name_tool -change i386/lib/libavutil.52.dylib @rpath/../Frameworks/libavutil.52.dylib libswscale.2.dylib
